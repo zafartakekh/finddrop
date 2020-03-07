ac8b5175e5fbe0e016b1a54220e8340d4cadadb8
zafartakekh <61861569+zafartakekh@users.noreply.github.com>
2020-03-07 15:53:25 +0600

    Update facenet.py

--- facenet.py
+++ facenet.py
@@ -1,50 +1,29 @@
-from keras import backend as K
-import time
-from multiprocessing.dummy import Pool
-K.set_image_data_format('channels_first')
-import cv2
 import os
 import glob
 import numpy as np
-from numpy import genfromtxt
+import cv2
 import tensorflow as tf
 from fr_utils import *
 from inception_blocks_v2 import *
-import win32com.client as wincl
+from keras import backend as K
 
 PADDING = 50
 ready_to_detect_identity = True
 windows10_voice_interface = wincl.Dispatch("SAPI.SpVoice")
 
+K.set_image_data_format('channels_first')
 FRmodel = faceRecoModel(input_shape=(3, 96, 96))
-
 def triplet_loss(y_true, y_pred, alpha = 0.3):
-    """
-    Implementation of the triplet loss as defined by formula (3)
-    
-    Arguments:
-    y_pred -- python list containing three objects:
-            anchor -- the encodings for the anchor images, of shape (None, 128)
-            positive -- the encodings for the positive images, of shape (None, 128)
-            negative -- the encodings for the negative images, of shape (None, 128)
-    
-    Returns:
-    loss -- real number, value of the loss
-    """
-    
     anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
-    
-    # Step 1: Compute the (encoding) distance between the anchor and the positive, you will need to sum over axis=-1
-    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), axis=-1)
-    # Step 2: Compute the (encoding) distance between the anchor and the negative, you will need to sum over axis=-1
-    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), axis=-1)
-    # Step 3: subtract the two previous distances and add alpha.
-    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
-    # Step 4: Take the maximum of basic_loss and 0.0. Sum over the training examples.
-    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
-    
-    return loss
 
+    pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor,
+               positive)), axis=-1)
+    neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, 
+               negative)), axis=-1)
+    basic_loss = tf.add(tf.subtract(pos_dist, neg_dist), alpha)
+    loss = tf.reduce_sum(tf.maximum(basic_loss, 0.0))
+   
+    return loss
 FRmodel.compile(optimizer = 'adam', loss = triplet_loss, metrics = ['accuracy'])
 load_weights_from_FaceNet(FRmodel)
 
